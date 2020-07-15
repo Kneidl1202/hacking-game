c1d5eacc3081cb7a716b1cbf9ae57ffb5ee1e7a3
Kneidl1202 <andreas.auer03@gmail.com>
2020-07-15 23:17:59 +0200

    made some changes to the gui

--- Hacknet_StartGui/Form1.cs
+++ Hacknet_StartGui/Form1.cs
@@ -16,5 +16,10 @@ namespace Hacknet_StartGui
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
